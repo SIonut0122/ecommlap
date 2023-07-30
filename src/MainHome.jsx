ec1bf62a16480b445aad8b61a05a3232441c7bb0
SIonut0122 <sionut0122@yahoo.com>
2023-07-30 12:22:25 +0300

    change basename path

--- src/MainHome.jsx
+++ src/MainHome.jsx
@@ -87,7 +87,7 @@ function MainHome() {
 
   
   return (
-<BrowserRouter basename="/ecommlap">
+<HashRouter>
     <div className='main_container'>
       <div className="main-cont-backdrop"></div>
       
@@ -136,7 +136,7 @@ function MainHome() {
       </section>
 
     </div>
-    </BrowserRouter>
+    </HashRouter>
 
   );
 }
